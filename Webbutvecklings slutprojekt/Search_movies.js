a3e3e3957b8a44d63ec721a7dc613bed6dc2b95b
Alexander Yhde <Alexander.yhde@skola.taby.se>
2024-05-13 13:32:28 +0200

    Version 5

--- Webbutvecklings slutprojekt/Search_movies.js
+++ Webbutvecklings slutprojekt/Search_movies.js
@@ -1,16 +1,28 @@
-var Sök_fält = document.getElementById("Sök_bar")
+var Sök_bar = document.getElementById("Sök_bar")
+var Resultat_div = document.getElementById("Resultat_div")
 
 Sök_bar.onkeydown = async function(event){
     if (event.key === "Enter"){
         event.preventDefault()
 
-        Resultat = sök_efter_film(Sök_bar.value)
-        
-        function sök_efter_film(Film_sökt){
+        var Resultat = await sök_efter_film(Sök_bar.value)
+        console.log(Resultat)
+        await Create_results(Resultat.results)
+        async function sök_efter_film(Film_sökt){
             let url = `https://api.themoviedb.org/3/search/movie?query=${Film_sökt}&api_key=d88a50faae8d9ba506d9df81d76f1c63`
-            let response = fetch(url);
-            let json = response.json(response)
-            console.log(json)
+            let response = await fetch(url);
+            let json = await response.json(response)
+            return json
+        }
+        
+        async function Create_results(Resultat){
+          for(let object = 0; object <= 4; object++){
+            console.log(Resultat[object])
+            let imageUrl = 'https://image.tmdb.org/t/p/w500' + Resultat[object].poster_path;
+            
+            console.log(imageUrl)
+            Resultat_div.insertAdjacentHTML('beforeend','<div id="Titlar_resultat">' + '<img src=' + imageUrl +  ' id="Resultat_bilder">' + Resultat[object].original_title + Resultat[object].release_date + "</div>")
+          }
         }
     }
 }
